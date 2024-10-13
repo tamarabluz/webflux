12cdcf3a56be0814e1beffa41880372fb3e0d1c3
Tamara <tamarabluz@gmail.com>
2024-10-13 19:42:50 -0300

    Criando entidade user

---
+++ webflux/src/main/java/tamara/webflux/entity/User.java
@@ -0,0 +1,20 @@
+package tamara.webflux.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+}
