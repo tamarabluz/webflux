cfffe73146517da809dce5b01a17448c2814f636
Tamara <tamarabluz@gmail.com>
2024-10-27 20:21:20 -0300

    adicionando exception handler para email duplicado

---
+++ webflux/src/main/java/tamara/webflux/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,38 @@
+package tamara.webflux.controller.exceptions;
+
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import reactor.core.publisher.Mono;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    ResponseEntity<Mono<StandardError>> duplicateKeyException(
+            DuplicateKeyException ex, ServerHttpRequest request
+    ) {
+        return ResponseEntity.badRequest()
+                .body(Mono.just(
+                        StandardError.builder()
+                                .timestamp(now())
+                                .status(BAD_REQUEST.value())
+                                .error(BAD_REQUEST.getReasonPhrase())
+                                .message(verifyDupKey(ex.getMessage()))
+                                .path(request.getURI().getPath())
+                                .build()
+                ));
+    }
+    private String verifyDupKey(String message){
+        if(message.contains("email dup key")){
+            return "Email already registered";
+        }
+        return "Dup key exception";
+    }
+}
---
+++ webflux/src/main/java/tamara/webflux/controller/exceptions/StandardError.java
@@ -0,0 +1,21 @@
+package tamara.webflux.controller.exceptions;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class StandardError implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private LocalDateTime timestamp;
+    private String path;
+    private Integer status;
+    private String error;
+    private String message;
+}
