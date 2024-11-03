84cec48d7a41cb6551544d438bda3bb73fbfba5a
Tamara <tamarabluz@gmail.com>
2024-11-03 15:32:26 -0300

    adicionando anotação personalizada para validar strings

--- webflux/src/main/java/tamara/webflux/model/request/UserRequest.java
+++ webflux/src/main/java/tamara/webflux/model/request/UserRequest.java
@@ -1,15 +1,22 @@
 package tamara.webflux.model.request;
 
 import jakarta.validation.constraints.*;
+import tamara.webflux.validation.TrimString;
 
 public record UserRequest(
 
+        @TrimString
         @NotBlank(message = "must not be null or empty")
         @Size(min=3, max = 50, message = "must be between 3 and 50 characters")
         String name,
+
+
+        @TrimString
         @NotBlank(message = "must not be null or empty")
         @Email(message = "invalid")
         String email,
+
+        @TrimString
         @NotBlank(message = "must not be null or empty")
         @Size(min=8, max = 50,  message = "must have at least 8 characters")
         @Pattern(regexp = "^(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
